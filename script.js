e4bf6c554d07674f1c4a2543d69f4ced4a159e7b
raphamd <207124200+raphamd@users.noreply.github.com>
2025-04-28 21:45:21 -0300

    feat: Added operate function to execute a operation

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const sum = (a,b) => a + b;
 const sub = (a,b) => a - b;
 const div = (a,b) => a / b;
 const mul = (a,b) => a * b;
+
+function operate(operator, foperand, soperand){
+  switch (operator){
+    case '+': return sum(foperand,soperand);
+    case '-': return sub(foperand,soperand);
+    case '/': return div(foperand,soperand);
+    case '*': return mul(foperand,soperand);
+  }
+}
